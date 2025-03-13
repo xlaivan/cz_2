7a7d5efd4a531d4bc5266eec2f6b496dd909d833
s33816 <s33816@pjwstk.edu.pl>
2025-03-13 09:13:32 +0100

    Zadanie_1

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main (String[] args) {
+
+        int[] arr1 = {1,2,3,4,5,6};
+
+
+        int[] arr = filterEvenIndex0ddValue(arr1);
+
+    }
+
+    public static int[] filterEvenIndex0ddValue(int[] array){
+        for (int i = 0; i < array.length; i++){
+            if (i % 2 == 0){
+                if (array[i] % 2 != 0){
+                    System.out.println(array[i]);
+                }
+            }
+        }
+        return array;
+    }
+
+}
